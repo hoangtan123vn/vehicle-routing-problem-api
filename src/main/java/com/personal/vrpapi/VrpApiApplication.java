c5ea24697f22803400cf979088c6bbb5724fcaed
hoangtan123vn <tanle271100@gmail.com>
2023-07-18 01:51:07 +0700

    init project

---
+++ src/main/java/com/personal/vrpapi/VrpApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.vrpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/vrpapi/VrpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.vrpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
